63b01ff6469e89aebd5af94cd101845723d8bb20
Seryoga <seryogasswot@gmail.com>
2022-09-17 22:09:01 +0300

    update setup.py add LICENSE add README add MANIFEST

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
-setup(name='clean_folder',
+setup(name='clean-folder',
       version='1',
       description='Сортуе файли, переносить та створюе папку під кожний тип файлу',
-      url='http://github.com/dummy_user/useful',
+      url='https://github.com/3Seryoga/clean_folder',
       author='3Seryoga',
       license='MIT',
-      packages=['useful'],
+      packages=find_namespace_packages(),
       entry_points={'console_scripts': ['clean-folder = clean_folder.clean:def main:']})
