483728eeaf40b8407a981eff11501454533abf34
Seryoga <seryogasswot@gmail.com>
2023-03-08 17:16:12 +0200

    a folder handler that sorts files in a specified folder by extension using multiple threads

--- clean_folder/clean.py
+++ clean_folder/clean.py
@@ -1,8 +1,8 @@
 import shutil
 import sys
 from pathlib import Path
+from threading import Thread
 
-# Словник з розширеннями для сортування
 EXTENSIONS_DICT = {
     'images': ('.jpeg', '.png', '.jpg', '.svg', '.dng'),
     'video': ('.avi', '.mp4', '.mov', '.mkv'),
@@ -16,15 +16,12 @@ TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", 
                "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
 trans = {}
 
-# За замовчуванням - None. Потім приймає шлях
+threads: list[Thread] = []
+
 main_folder: Path | None = None
 
 
 def main():
-    """Перевіряємо дійсність і існування шляху указаного користувачем
-        Повертаємо запуск наступної функції - cleaner.
-        root_folder - так записуємо шлях до папки котру будемо чистити та сортувати.
-    """
     global main_folder
 
     if len(sys.argv) < 2:
@@ -32,7 +29,6 @@ def main():
         exit()
 
     root_folder = Path(sys.argv[1])
-    # root_folder => Path("D:\Maya\Desktop\Хлам")
 
     if (not root_folder.exists()) or (not root_folder.is_dir()):
         print('Path incorrect')
@@ -44,69 +40,45 @@ def main():
 
 
 def cleaner(folder: Path):
-    """Приймає на вхід шлях до папки котру будемо перевіряти.
-        Перевіряє всі входження в директорію - чи це файл чи директорія.
-        І для директорій рекурсивно заходить в них та так само їх перевіряє.
-    """
     for file in folder.iterdir():
-
         if file.is_file():
-            # Будемо передавати цей файл далі на сортування і переміщення до нової папки.
             sort_file(file)
 
         if file.is_dir():
-            # Будемо заходити всередину папки та повертати її вміст знову в середину цієї ж функції.
-            # Надалі видаляти пусту папку.
+            folder_thread = Thread(target=cleaner, args=(file,))
+            folder_thread.start()
+            threads.append(folder_thread)
 
             cleaner(file)
-
-            # Видаляємо директорію, якщо вона порожня
-            if not any(file.iterdir()):
-                file.rmdir()
 
 
 def sort_file(file: Path):
     file_suffix = file.suffix.lower()
-    # Приберемо розширення(extension) з імені файлу
     file_name = file.stem
 
-    # key => music, values => ['.mp3', '.ogg', '.wav', '.amr']
     for key, values in EXTENSIONS_DICT.items():
         if file_suffix in values:
-            # Відправляємо ім'я файлу в функцію normalize() для нормалізації імені
             normalized_file_name = normalize(file_name)
             new_file_name = normalized_file_name + file_suffix
-            # Переміщуємо в нову папку, яка відповідає ключу
             end_folder = main_folder.joinpath(key)
-
-            # Створюємо папку по даному шляху якщо її не існує. end_folder 
             end_folder.mkdir(exist_ok=True)
             new_file_path = end_folder.joinpath(new_file_name)
             file.rename(new_file_path)
 
             if key == 'archives':
-                # Шлях до папки з відкритим архівом
-                base_archive_dir = end_folder.joinpath(normalized_file_name)  # Створюємо шлях
-                base_archive_dir.mkdir(exist_ok=True)  # Створюємо папку
+                base_archive_dir = end_folder.joinpath(normalized_file_name)
+                base_archive_dir.mkdir(exist_ok=True)
 
                 shutil.unpack_archive(new_file_path, base_archive_dir)
 
 
 def fill_translate():
-    """
-    Створює MAP для перекладу і заповнює змінну trans
-    """
     for cyril, latin in zip(CYRILLIC_SYMBOLS, TRANSLATION):
         trans[ord(cyril)] = latin
         trans[ord(cyril.upper())] = latin.upper()
 
 
 def normalize(file_name: str) -> str:
-    """
-    Замінює неліквідні знаки на "_" та кириличні букви на латинські
-    :param file_name: Ім'я файлу без розширення та шляху
-    :return: нормалізоване ім'я без розширення
-    """
     normalized_name = file_name.translate(trans)
 
     for i in normalized_name:
@@ -118,5 +90,7 @@ def normalize(file_name: str) -> str:
 
 if __name__ == '__main__':
     main()
+    for thread in threads:
+        thread.join()
     print("Сompleted!!!")
     exit()
